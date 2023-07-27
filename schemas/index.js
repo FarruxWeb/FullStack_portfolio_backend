96d1242eefc73b3a81642a7e5c300be6afd2dc2b
Farrux <farrukholimov05@gmail.com>
2023-07-27 15:12:25 +0500

    First commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,14 @@
-export const schemaTypes = []
+import testimonials from './testimonials';
+import abouts from './abouts';
+import works from './works';
+import skills from './skills';
+import brands from './brands';
+import experiences from './experiences';
+import workExperience from './workExperience';
+import contact from './contact';
+
+export const schemaTypes = [
+    testimonials, abouts, works, skills, brands, experiences,
+    workExperience, contact
+]
+
